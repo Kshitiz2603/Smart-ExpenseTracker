ee1371cd0cbf2f457c85585b697df43d0e8d53f3
Kshitiz2603 <kshitiz2603@gmail.com>
2024-10-14 23:56:44 +0530

    Login Page Css handling

--- frontend/src/app/@sys/Login/Login.jsx
+++ frontend/src/app/@sys/Login/Login.jsx
@@ -1,56 +1,66 @@
-import React from 'react'
-import './Login.css';
-import {Form, Button, Select, Checkbox, DatePicker, Input} from 'antd';
+import React from "react";
+import "./Login.css";
+import { Form, Button, Select, DatePicker, Input } from "antd";
 
 function Login() {
   return (
     <>
-    <div className='login'>
-        <img src='' alt=''></img>
-        <div className='login-header'>
-            <h1>Kshitiz Choudhary</h1>
+      <div className="login">
+        <div className="login-header">
+          <h1>Create a Account</h1>
         </div>
-        <div className='login-hyperlink'>
-            <h4>Already a member ? Login</h4>
+        <div className="login-hyperlink">
+          <h4>Already a member ? Login</h4>
         </div>
-        <div className='login-info'>
-            
-            <Form>
-                <Form.Item name = "fullName" label= "Full Name">
-                    <Input placeholder='Type your name'/>
-                </Form.Item>
+        <div className="login-info">
+          <Form
+            name="basic"
+            labelCol={{ span: 10 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 600 }}
+            initialValues={{ remember: true }}
+            autoComplete="off"
+            layout={"vertical"}
+          >
+            <Form.Item name="fullName" label="Full Name">
+              <Input placeholder="Type your name" style={{ width: 300 }}/>
+            </Form.Item>
 
-                <Form.Item name = "email" label= "Email">
-                    <Input placeholder='Type your Email'/>
-                </Form.Item>
+            <Form.Item name="email" label="Email">
+              <Input placeholder="Type your Email" style={{ width: 300 }}/>
+            </Form.Item>
 
-                <Form.Item name = "password" label= "Password">
-                    <Input.Password placeholder='Type your password'/>
-                </Form.Item>
+            <Form.Item name="password" label="Password">
+              <Input.Password placeholder="Type your password" style={{ width: 300 }}/>
+            </Form.Item>
 
-                <Form.Item name = "confirmPassword" label= "Confirm Password">
-                    <Input.Password placeholder='Confirm Passowrd'/>
-                </Form.Item>
+            <Form.Item name="confirmPassword" label="Confirm Password">
+              <Input.Password placeholder="Confirm Passowrd" style={{ width: 300 }}/>
+            </Form.Item>
 
-                <Form.Item name = "gender" label= "Gender">
-                    <Select placeholder="Select Your Gender"/>
-                    <Select.Option value="male">Male</Select.Option>
-                    <Select.Option value="female">Female</Select.Option>
-                </Form.Item>
+            <Form.Item name="gender" label="Gender">
+              <Select placeholder="Select Your Gender" style={{ width: 300 }}/>
+              <Select.Option value="male">Male</Select.Option>
+              <Select.Option value="female">Female</Select.Option>
+            </Form.Item>
 
-                <Form.Item name = "dob" label= "Date of Birth">
-                    <DatePicker picker="date" placeholder='Select your Date of birth'/>
-                </Form.Item>
+            <Form.Item name="dob" label="Date of Birth">
+              <DatePicker
+                picker="date"
+                placeholder="Select your Date of birth"
+              />
+            </Form.Item>
 
-                <Button type="primary">Primary Button</Button>
-            </Form>
-        <div className='login-submit-btn'>
-            <button>SIGN UP</button>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
         </div>
-        </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
