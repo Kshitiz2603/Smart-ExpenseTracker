8b3999055ef88b69d9d06f2cbdd7c462abef479d
Kshitiz2603 <kshitiz2603@gmail.com>
2024-10-16 23:39:07 +0530

    Main App File Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,31 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Home from './app/@sys/Home/Home'
-import Header from './app/@sys/Header/Header';
-import Login from './app/@sys/Login/Login';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./app/@sys/Home/Home";
+import Header from "./app/@sys/Header/Header";
+import Login from "./app/@sys/Login/Login";
+import Profile from "./app/@sys/Profile/Profile";
+import { Footer } from "antd/es/layout/layout";
+import Dashboard from "./app/@sys/Dashboard/Dashboard";
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header/>
-      <Routes>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/home' element={<Home/>}/>
-      </Routes>
-    </BrowserRouter>
-  )
+    <>
+      <div className="rootPage-wrapper">
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
